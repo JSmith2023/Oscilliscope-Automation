8b99731f3857cba76602d470211f4c7377ded478
muhaad <63682537+muhaad@users.noreply.github.com>
2022-07-25 10:42:56 -0400

    Update Peak_Finder.py
    Added cut off/cut on frequency variables to specify range to look for peak. Functionally works for any range of frequencies.

--- Peak_Finder.py
+++ Peak_Finder.py
@@ -5,21 +5,23 @@ import numpy as np
 
 
 
-y_origin=-78.6279          ##This parameters are only accurate based on the setting of the oscilliscope (preset#10 saved in setups)
-x_origin=0.0
-y_inc=0.812698
-x_inc=4768.37158203125
+y_origin= -58.6279
+x_origin=  0.0
+y_inc= 0.812698
+x_inc=  15258.7890625
+
+
+cuton_freq = 130*10**6
+cutoff_freq = 160*10**6
 
 time=[]
 ROC=[]
 
+input("Continue")
 
-
-input("stop")
-
-with open('function2.txt', 'r') as f:
+with open('C:/Users/muhaa/OneDrive/Desktop/Test_Env/function2.txt', 'r') as f:
     for line in f:
-        #max_freq=0                                     #max_freq is the INDEX at which the peak frequency of the ROC is
+
         freq=[]
         pwr=[]
         data=line.split(' ')
@@ -28,66 +30,52 @@ with open('function2.txt', 'r') as f:
         for i in range(length):
             freq.append(float(i)*x_inc+x_origin)
             pwr.append(float(data[i])*y_inc+y_origin)
-        ROC_peak=0                                      #ROC_peak is the INDEX at which the peak frequency of the ROC is
-        peak_width=150
-        #print(freq[0])
-        while(1):
-            peaks=find_peaks(pwr, width=peak_width, distance=200)       #distance= 0.95 MHz
-            try:
-                ROC_peak=peaks[0][0]
-            except:
-                "nothing to do here"
-            
-            for i in peaks[0]:
-                if(abs(freq[ROC_peak]-94*(10**6))>abs(freq[i]-94*(10**6))):
-                        ROC_peak=i
-            if(freq[ROC_peak]>95*(10**6) or freq[ROC_peak]<93*(10**6)):
-                peak_width-=5
-            elif(peak_width<0):
-               print("error: peak width 0 at: ", time[len(time)-1])
-               input("continue?")
-               break
-            else:
-               break
-            
+     
+        peaks=find_peaks(pwr)[0]
+        ROC_peak = None
+      
+        for i in peaks:
+            print(freq[i])
+            if(freq[i]<cutoff_freq and freq[i]> cuton_freq):
+                print("Valid peak at: ", freq[i])
+                if(ROC_peak == None or pwr[i]>pwr[ROC_peak]):
+                    print("peak_found at: ", freq[i])
+                    ROC_peak=i
+                   
+        if(ROC_peak == None):
+            print("Error, no valid peak within range found")
         
-        if(freq[ROC_peak]>95*(10**6) or freq[ROC_peak]<93*(10**6) or True):                 #just some error checking
-            print("error: peak out of expected range at: ", time[len(time)-1])
+        elif(freq[ROC_peak]>cutoff_freq or freq[ROC_peak]<cuton_freq):                 #just some error checking
+            print("error: peak out of expected range at: ", time[len(time)-1], "seconds")
             print("peak calculated at",  freq[ROC_peak], "Hz")
-            print("index: ", ROC_peak)
-            print("width: ", peak_width)
-            peaks=find_peaks(pwr, width=peak_width, distance=200)
-            for i in peaks[0]:
-                print(freq[i])
+            peaks=find_peaks(pwr)[0]
             plt.plot(freq, pwr)
-            plt.axvline(x = freq[i], color = 'b', label = 'axvline - full height')
+            plt.axvline(x = freq[ROC_peak], color = "red", label = 'axvline - full height')
+            plt.ylabel("dBm")
+            plt.xlabel("Frequency")
             plt.show()
-            input("continue")
+   
 
             
         ROC.append(freq[ROC_peak])
         
-            
-   
-        
+          
+input("Done: press enter to save data")
 
-print("done")
-
-def app():
-
-    with open('ROC.txt', 'a') as f:
-        for i in ROC:                      
-            f.write("%s " % i)
+with open('C:/Users/muhaa/OneDrive/Desktop/Test_Env/ROC.txt', 'a') as f:
+    for i in ROC:                      
+        f.write("%s " % i)
+    print("data saved to ROC")
 
 
-    with open('time.txt', 'a') as f:
-        for i in time:                      
-            f.write("%s " % i)
+with open('C:/Users/muhaa/OneDrive/Desktop/Test_Env/time.txt','a') as f:
+    for i in time:                      
+        f.write("%s " % i)
+    print("data saved to ROC")
 
       
     
-
-plt.plot(time,ROC, linewidth=.1)
+plt.plot(time,ROC, linewidth=.1)            #plots ROC frequency vs time. Ensure that it looks correct
 plt.xticks
 plt.show()
         
@@ -101,3 +89,4 @@ plt.show()
         
         
         
+
